53df7bf3b15edd93ad1c0323286eeb0a81d0f454
sohyeon <sohyoun26@gmail.com>
2025-02-28 10:21:53 +0900

    add

---
+++ farmstory/src/main/java/kr/co/farmstory/controller/profile/UpdateinfoController.java
@@ -0,0 +1,30 @@
+package kr.co.farmstory.controller.profile;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/profile/updateinfo.do")
+public class UpdateinfoController extends HttpServlet{
+
+	private static final long serialVersionUID = -3568609080669717772L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/profile/update-info.jsp");
+		dispatcher.forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	
+
+}
