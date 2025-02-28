c724bd211f06627b8c087159a58432acb017551c
sjo112777 <sjo112777@gmail.com>
2025-02-28 18:53:43 +0900

    add farmstory

--- farmstory/src/main/java/kr/co/farmstory/util/SQL.java
+++ farmstory/src/main/java/kr/co/farmstory/util/SQL.java
@@ -30,7 +30,85 @@ public class SQL {
 	public static final String SEARCH_USER = "SELECT `name`, `uid`, `email`, `regDate` "
 											+ "FROM `user` "
 											+ "WHERE `name`=? AND `email`=?";
-						
+
+	// community
+		public static final String SELECT_MAX_NO = "SELECT MAX(`no`) FROM `Community`";
+		public static final String SELECT_COUNT_COMMUNITY = "SELECT COUNT(*) FROM `Community`";
+		public static final String SELECT_COMMUNITY_WITH_FILE = "select "
+																+ "a.*,"
+																+ "f.*,"
+																+ "u.`nick` "
+																+ "FROM `Community` AS a "
+																+ "LEFT JOIN `file` AS f ON a.no = f.ano "
+																+ "JOIN `user` AS u ON a.writer = u.uid "
+																+ "where a.`no`=?";
+			
+		public static final String SELECT_ALL_COMMUNITY = "SELECT "
+														+ "a.*,"
+														+ "u.`nick` "
+														+ "FROM `Community` AS a "
+														+ "JOIN `user` AS u "
+														+ "ON a.writer = u.uid "
+														+ "ORDER BY `no` desc "
+														+ "LIMIT ?, 10";
+		
+		public final static String SELECT_ALL_COMMUNITY_BY_SEARCH = "SELECT "
+																	+ "a.*, "
+																	+ "u.`nick` "
+																	+ "FROM `Community` AS a "
+																	+ "JOIN `user` AS u ON a.writer = u.uid ";
+		
+		public final static String SELECT_COUNT_COMMUNITY_FOR_SEARCH = "select count(*) from `Community` as a ";
+		public final static String JOIN_FOR_SEARCH_NICK  = "JOIN `user` as u ON a.writer = u.uid ";
+		public final static String WHERE_FOR_SEARCH_TITLE   = "WHERE `title` LIKE ? ";
+		public final static String WHERE_FOR_SEARCH_CONTENT = "WHERE `content` LIKE ? ";
+		public final static String WHERE_FOR_SEARCH_WRITER  = "WHERE `nick` LIKE ? ";	
+		public final static String ORDER_FOR_SEARCH  = "ORDER BY `no` DESC ";
+		public final static String LIMIT_FOR_SEARCH  = "LIMIT ?, 10";
+																
+		
+		public static final String INSERT_COMMUNITY = "insert into `Community` set "
+														+ "`title`=?,"
+														+ "`content`=?,"
+														+ "`file`=?,"
+														+ "`writer`=?,"
+														+ "`regip`=?,"
+														+ "`wdate`=NOW()";
+		
+		// comment
+		public static final String INSERT_COMMENT = "insert into `comment` set "
+													+ "`parent`=?,"
+													+ "`content`=?,"
+													+ "`writer`=?,"
+													+ "`regip`=?,"
+													+ "`wdate`=NOW()";
+		
+		public static final String SELECT_ALL_COMMENT_BY_PARENT = "SELECT "
+																	+ "c.*, "
+																	+ "u.`nick` "
+																	+ "FROM `comment` AS c "
+																	+ "JOIN `user` AS u ON c.writer = u.uid "
+																	+ "WHERE `parent`=? "
+																	+ "ORDER BY `cno` ASC";
+		
+		public static final String SELECT_COMMENT_BY_CNO = "SELECT "
+															+ "c.*, "
+															+ "u.`nick` "
+															+ "FROM `comment` AS c "
+															+ "JOIN `user` AS u ON c.writer = u.uid "
+															+ "WHERE `cno`=?";
+		
+		// file
+		public static final String INSERT_FILE = "insert into `file` set "
+													+ "`ano`=?,"
+													+ "`oName`=?,"
+													+ "`sName`=?,"
+													+ "`rdate`=NOW()";
+		
+		public final static String SELECT_FILE_BY_FNO = "select * from `file` where `fno`=?";
+		public final static String UPDATE_FILE_DOWNLOAD_COUNT = "UPDATE `file` SET `download` = `download` + 1 WHERE `fno`=?";
+		
+	}
 			
 	
-}
+
